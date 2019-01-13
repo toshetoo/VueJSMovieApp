88593ce704e4aeec34fbd614a445325cb8f99ec8
Todor Ivanov <fmi@fmi.uni-plovdiv.bg>
2019-01-13 15:31:27 +0200

    Add listing of dogs

--- src/components/Dogs/DogsHolder.vue
+++ src/components/Dogs/DogsHolder.vue
@@ -1,26 +1,47 @@
 <template>
   <div class="container">
-    {{ dogs }}
+    <ListItem v-for="(dog) in getDogs"
+      :key="dog.guid"
+      :guid="dog.guid"
+      :isSold="dog.isSold"
+      :picture="dog.picture"
+      :age="dog.age"
+      :eyeColor="dog.eyeColor"
+      :name="dog.name"
+      :gender="dog.gender"
+      :about="dog.about"
+    >
+    </ListItem>
   </div>
 </template>
 
 <script>
   import dogsService from '../../api/services/dogsService';
+  import ListItem from './ListItem';
 
   export default {
     name: 'DogsHolder',
+    components: {
+      ListItem,
+    },
     data() {
       return {
         dogs: [],
       };
     },
+    computed: {
+      getDogs() {
+        return this.dogs;
+      }
+    },
     created() {
-      this.getDogs();
+      this.getAllDogs();
     },
     methods: {
-      getDogs() {
+      getAllDogs() {
         dogsService.getAllDogs().then((dogs) => {
-          this.dogs = dogs;
+          this.dogs = dogs.data;
+          console.log(dogs);
         });
       },
     },
---
+++ src/components/Dogs/ListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card d-inline-flex ml-2 mr-2 mt-2 mb-2" style="width: 18rem;">
+    <img class="card-img-top" :src="picture" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Name: {{ name }}</h5>
+      <p class="card-text">{{ about }}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Age: {{ age }}</li>
+      <li class="list-group-item">Eye color: {{ eyeColor }}</li>
+      <li class="list-group-item">Gender: {{ gender }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ListItem',
+    props: {
+      guid: String,
+      isSold: Boolean,
+      picture: String,
+      age: Number,
+      eyeColor: String,
+      name: String,
+      gender: String,
+      about: String,
+    },
+  };
+</script>
